7352faf5ace8c1e2dc4a51cea1496a848f6948db
bokkun <bokkun.moeanimecharacter.byt@gmail.com>
2018-10-11 01:45:55 +0900

    デザイン変更
    ヘッダー画像のサイズ変更(PHP)

--- common/header.php
+++ common/header.php
@@ -2,13 +2,13 @@
     <div align="left">
     <?php $img="0113.png"; ?>
         <div>
-        <a href="<?php $url ?>"><img src="<?php $url ?>public/client/image/<?php echo $img; ?>" width="40" height="40"></a>
+        <a href="<?php $url ?>"><img class='top-image' src="<?php $url ?>public/client/image/<?php echo $img; ?>"></a>
         <strong>
             <em>Bokkun's homepage</em> <br/>
             <div class="top" align="center"><?php if (isset($title)){ echo $title; }else{ echo 'ぼっくんのホームページ';} ?></div>
         </strong>
         </div>
-    </div> 
+    </div>
 
     <div class='date' align="right"></div>
     <div class='time' align="right"></div>
