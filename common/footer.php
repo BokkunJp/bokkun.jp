bfc7c956a6f6737a2a0a0c4e5e6ed8e4c544847c
bokkun <private.mail@bokkun.jp>
2019-02-12 03:08:56 +0900

    エラーページの年号の変更

--- common/footer.php
+++ common/footer.php
@@ -3,5 +3,5 @@
     <div align='center'>Bokkun's Page</div>
             <div class="footer_char" align='right'>本ホームページは、<a href="https://www.value-domain.com">バリュードメイン様</a>のレンタルサーバを用いて作成しています。</div>
     <div class="twitter" align='right'><a href="https://twitter.com/intent/tweet?screen_name=bokkun_jp_xyz" class="twitter-mention-button" data-show-count="false">Tweet to @bokkun_jp_xyz</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script></div>
-    <small class="copy_right" align='left'>Copyright © 2016-2018 Bokkun All rights reserved.</small>
+    <small class="copy_right" align='left'>Copyright © 2016-2019 Bokkun All rights reserved.</small>
 </footer>
