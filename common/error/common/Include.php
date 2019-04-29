7d5f2e32eaa8451d7446b876140e85ee0951b66e
ぼっくん <bokkun.moeanimecharacter.byt@gmail.com>
2019-04-29 09:16:15 +0900

    Vue.jsのパスがおかしかったので修正
    https://が入っていたので外した。

--- common/error/common/Include.php
+++ common/error/common/Include.php
@@ -6,7 +6,7 @@
 require_once(__DIR__. "/Function/UA.php");
 echo "<script src='//code.jquery.com/jquery-3.4.0.min.js'></script>";
 echo "<script src='//code.jquery.com/ui/1.12.0/jquery-ui.min.js' integrity='sha256-eGE6blurk5sHj+rmkfsGYeKyZx3M4bG+ZlFyA7Kns7E='crossorigin='anonymous'></script>";
-echo "<script src='https: //cdn.jsdelivr.net/npm/vue'></script>";
+echo "<script src='//cdn.jsdelivr.net/npm/vue'></script>";
 
 
 /**
--- public/common/Load/include.php
+++ public/common/Load/include.php
@@ -2,9 +2,9 @@
 if ($ua->DesignJudege() === PC_design) {
     echo "<script src='//code.jquery.com/jquery-3.4.0.min.js'></script>";
     echo "<script src='//code.jquery.com/ui/1.12.0/jquery-ui.min.js' integrity='sha256-eGE6blurk5sHj+rmkfsGYeKyZx3M4bG+ZlFyA7Kns7E='crossorigin='anonymous'></script>";
-    echo "<script src='https: //cdn.jsdelivr.net/npm/vue'></script>";
+    echo "<script src='//cdn.jsdelivr.net/npm/vue'></script>";
 } else if ($ua->DesignJudege() === SP_design) {
     echo "<script src='//code.jquery.com/jquery-3.4.0.min.js'></script>";
     echo "<script src='//code.jquery.com/ui/1.12.0/jquery-ui.min.js' integrity='sha256-eGE6blurk5sHj+rmkfsGYeKyZx3M4bG+ZlFyA7Kns7E='crossorigin='anonymous'></script>";
-    echo "<script src='https: //cdn.jsdelivr.net/npm/vue'></script>";
+    echo "<script src='//cdn.jsdelivr.net/npm/vue'></script>";
 }
