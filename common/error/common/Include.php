0a26bd743c70b05c3fde1a6a1a1e9ece458a2938
ぼっくん <bokkun.moeanimecharacter.byt@gmail.com>
2019-04-29 09:13:05 +0900

    エラーページでjQueryが動かなかったので修正
    前回のコミットで、jQueryの機能を使わない設定にしてしまっていた。

--- common/error/common/Include.php
+++ common/error/common/Include.php
@@ -4,6 +4,10 @@
  */
 // 必要なファイルの読み込み
 require_once(__DIR__. "/Function/UA.php");
+echo "<script src='//code.jquery.com/jquery-3.4.0.min.js'></script>";
+echo "<script src='//code.jquery.com/ui/1.12.0/jquery-ui.min.js' integrity='sha256-eGE6blurk5sHj+rmkfsGYeKyZx3M4bG+ZlFyA7Kns7E='crossorigin='anonymous'></script>";
+echo "<script src='https: //cdn.jsdelivr.net/npm/vue'></script>";
+
 
 /**
  * こちらの機能はエラーページでは使用しない
