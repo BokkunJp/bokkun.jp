880a957a7e40ddbf90b72bae206d4655be40566a
bokkun <private.mail@bokkun.jp>
2019-09-08 03:49:57 +0900

    ヘッダー・フッター自動表示用のシステム構築
    カスタム画面の調整

--- public/custom_test/Layout/footer.php
+++ public/custom_test/Layout/footer.php
@@ -1,7 +1,8 @@
-
-    <footer class='footer'>
+<?php $year = $siteConfig['footer']->GetYear(); ?>
+<footer class='footer'>
     <div align='center'>Bokkun's Page</div>
-            <div class="footer_char" align='right'>本ホームページは、<a href="https://www.value-domain.com">バリュードメイン様</a>のレンタルサーバを用いて作成しています。</div>
-    <div class="twitter" align='right'><a href="https://twitter.com/intent/tweet?screen_name=bokkun_jp_xyz" class="twitter-mention-button" data-show-count="false">Tweet to @bokkun_jp_xyz</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script></div>
-    <small class="copy_right" align='left'>Copyright © 2016-2018 Bokkun All rights reserved.</small>
-</footer>
+    <div class="footer_char" align='right'>本ホームページは、<a href="https://www.value-domain.com">バリュードメイン様</a>のレンタルサーバを用いて作成しています。</div>
+    <div class="twitter" align='right'><a href="https://twitter.com/intent/tweet?screen_name=bokkun_jp_xyz" class="twitter-mention-button" data-show-count="false">Tweet to @bokkun_jp_xyz</a>
+        <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
+    <small class="copy_right" align='left'>Copyright © <?= $year['start'] ?>-<?= $year['end'] ?> Bokkun All rights reserved.</small>
