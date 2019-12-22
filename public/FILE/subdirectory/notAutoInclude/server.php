30c67a96096cc52c573cde074342c4f1db11c978
bokkun <private.mail@bokkun.jp>
2019-12-22 19:35:28 +0900

    FILE処理の修正
    バリデートエラーをファイルページ内に表示できるように変更。
    これに伴い、セッションエラー以外のエラー時に別画面に遷移することはなくなった。
    =============================
    コミット漏れ修正

--- public/FILE/subdirectory/notAutoInclude/server.php
+++ public/FILE/subdirectory/notAutoInclude/server.php
@@ -15,7 +15,8 @@ require_once dirname(__DIR__). '/File.php';
 
 echo '<div class=\'contents\' />';
 
-CheckToken('token', '不正な値が送信されました。<br/>');
+CheckToken('token', '不正な値が送信されました。<br/>', false);
+
 if (!isset($session)) {
     $session = new PublicSetting\Session();
 }
@@ -28,27 +29,30 @@ if (!empty(PublicSetting\Setting::GetQuery('mode')) && PublicSetting\Setting::Ge
     if ($count > COUNT_START) {
         DeleteImage();
     } else {
-        echo '削除対象が選択されていないか、画像がありません。<br/>';
+        $session->Delete();
+        $session->Add('notice', '削除対象が選択されていないか、画像がありません。');
+    }
+    if (!$session->Judge('notice')) {
+        $session->Add('success', ($count - COUNT_START). '件の画像の削除に成功しました。');
     }
 
 } else {
     if (isset($file['file']) && is_uploaded_file($file['file']['tmp_name'])) {
-        ImportImage($file);
+        $result = ImportImage($file);
+        if ($result === true) {
+            $session->Add('success', '画像のアップロードに成功しました。');
+        } else if ($result === false) {
+            $session->Add('notice', '画像のアップロードに失敗しました。');
+        } else if ($result === -1) {
+            $session->Add('notice', '画像ファイル以外はアップロードできません。');
+        }
     } else {
-        echo 'ファイルが存在しません。<br/>';
+        $session->Add('notice', 'ファイルが存在しません。');
     }
 }
-
 @session_regenerate_id();
 $session->Add('token', sha1(session_id()));
 // $session->FinaryDestroy();
+$url = new PublicSetting\Setting();
+header('Location:' . $url->GetUrl('public/FILE'));
 ?>
-
-<br />
-<a href='javascript:location.href = location;'>ファイル選択ページへ戻る</a>
-</div>
-        <div>
-            <?php require_once __DIR__. '/Layout/footer.php'; ?>
-        </div>
-    </body>
-</html>
