c7e447b66c0de189dbeee4f64201d967e931e0a2
bokkun <private.mail@bokkun.jp>
2020-01-25 20:56:07 +0900

    ファイル整理
    不要なファイルの削除

--- private/FILE/subdirectory/notAutoInclude/Layout/footer.php
+++
@@ -1,7 +0,0 @@
-
-    <footer class='footer'>
-    <div align='center'>Bokkun's Page</div>
-            <div class="footer_char" align='right'>本ホームページは、<a href="https://www.value-domain.com">バリュードメイン様</a>のレンタルサーバを用いて作成しています。</div>
-    <div class="twitter" align='right'><a href="https://twitter.com/intent/tweet?screen_name=bokkun_engineer" class="twitter-mention-button" data-show-count="false">Tweet to @bokkun_engineer</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script></div>
-    <small class="copy_right" align='left'>Copyright © 2016-2019 Bokkun All rights reserved.</small>
-</footer>
--- private/FILE/subdirectory/notAutoInclude/Layout/require.php
+++
@@ -1,6 +0,0 @@
-<?php
-ini_set('error_reporting', E_ALL | ~E_STRICT);
-require_once dirname(dirname(dirname(dirname(__DIR__)))) . '/common/Setting.php';
-require_once PRIVATE_COMPONENT_DIR . '/UA.php';
-require_once PRIVATE_COMMON_DIR . "/Token.php";
-require_once PRIVATE_COMMON_DIR . "/Include.php";
