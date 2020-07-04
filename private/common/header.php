5e1844360866507529062930cb01fcdacced71b6
bokkun <private.mail@bokkun.jp>
2020-07-05 02:05:52 +0900

    【管理側|全般】トップアイコンクリック時の遷移先変更
    トップアイコンクリック時の遷移先を公開側トップページから、管理側一覧ページへ変更する。
    (管理側一覧ページの時は、公開側のトップページへ戻る)

--- private/common/header.php
+++ private/common/header.php
@@ -1,8 +1,14 @@
+<?php
+$imgUrl = $url;
+if (strpos(basename(getcwd()), 'private') === false) {
+    $url = AddPath(AddPath($url, 'private'), 'admin.php', false);
+}
+?>
 <header class='header'>
     <div align="left">
         <?php $img = "pc15.gif"; ?>
         <div>
-            <a href="<?php echo $url; ?>"><img src="<?php echo $url; ?>/private/client/image/<?php echo $img; ?>" width="40" height="40"></a>
+            <a href="<?php echo $url; ?>"><img src="<?php echo $imgUrl; ?>/private/client/image/<?php echo $img; ?>" width="40" height="40"></a>
             <link rel="shortcut icon" href="<?php echo $url; ?>/private/client/image/5959715.png">
             <strong>
                 <em>Bokkun's homepage<?= $siteConfig['header']->GetVersion() ?></em> <br />
