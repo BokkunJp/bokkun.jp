8b5f1e02ba16c48acb3608e9707c47d7ff26b7c1
bokkun <private.mail@bokkun.jp>
2020-07-06 00:23:11 +0900

    修正。

--- public/MAIL/design.php
+++ public/MAIL/design.php
@@ -1,7 +1,7 @@
 <?php
 //SendMail(['bokkun.moeanimecharacter.byt@gmail.com', 'テスト送信', 'ウェザサイトからの送信です。', 'From: private.mail@bokkun.jp' . "\r\n"]);
 ?>
-<form enctype="multipart/form-data" action=".//" method='POST'>
+<form enctype="multipart/form-data" action="./" method='POST'>
     <input type='hidden' name='token' value="<?= $token = MakeToken() ?>" />
     <input type='textbox' name='title' />
     <br />
