25fb257189c2de92df0652b613b7c2889ed66e88
ぼっくん <bokkun.moeanimecharacter.byt@gmail.com>
2020-10-18 18:41:54 +0900

    vue.jsのパス修正 開発と本番で読み込むファイルを分ける

--- common/error/common/Include.php
+++ common/error/common/Include.php
@@ -6,7 +6,11 @@
 require_once(__DIR__ . "/Function/UA.php");
 echo "<script src='//code.jquery.com/jquery-3.4.1.min.js'></script>";
 echo "<script src='//code.jquery.com/ui/1.12.0/jquery-ui.min.js' integrity='sha256-eGE6blurk5sHj+rmkfsGYeKyZx3M4bG+ZlFyA7Kns7E='crossorigin='anonymous'></script>";
-echo "<script src='//jp.vuejs.org/js/vue.js'></script>";
+if (PublicSetting\Setting::GetServarName() === 'bokkun.jp.project') {
+    echo "<script src='//jp.vuejs.org/js/vue.js'></script>";
+} else {
+    echo "<script src='//jp.vuejs.org/js/vue.min.js'></script>";
+}
 
 
 /**
