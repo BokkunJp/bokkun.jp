a0dca793e0db3eb12b7f58168d52e4cc3d895720
ぼっくん <private.mail@bokkun.jp>
2021-01-09 16:29:13 +0900

    【バグ|公開側CSV作成】ファイル名に特定の文字をセットでFatal Error
    preg_matchの条件を調整した。

--- common/CSV/BaseModel.php
+++ common/CSV/BaseModel.php
@@ -159,8 +159,8 @@ class CSV_Base {
             // a-z, A-Z, 0-9, 日本語のみをファイル名に用いることができるとする (特殊文字は不可)
             mb_regex_encoding('UTF-8');
 
-            if (preg_match("/[a-zA-Z0-9-_-ぁ-んァ-ヶー一-龠]$/", $haystack)) {
-                if (!preg_match("/[a-zA-Z0-9-_-ぁ-んァ-ヶー一-龠]+\.{$extensiton}$/", $haystack)) {
+            if (preg_match("/^[a-zA-Z0-9-_-ぁ-んァ-ヶー一-龠]+$/", $haystack)) {
+                if (preg_match("/^[a-zA-Z0-9-_-ぁ-んァ-ヶー一-龠]+\.{$extensiton}$/", $haystack)) {
                     $ret = EXTENSION_NONE_TRUE;
                 } else {
                     $ret = true;
