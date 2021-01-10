b13905cd70a9cf20ace2bdef91a20c20546ed0df
ぼっくん <private.mail@bokkun.jp>
2021-01-11 06:23:06 +0900

    BOKKUN-38 【公開側|画像編集ページ】デザイン調整
    画像を5枚ずつ横並びにし、さらに画像のマウスオーバー(画像タップ)で吹き出しの形で情報を表示するようにする。

--- IMAGE/subdirectory/View.php
+++ IMAGE/subdirectory/View.php
@@ -12,20 +12,22 @@
 function ViewImage($imageName, $imageUrl, $fileTime) {
 //    $imageHtml = new CustomTagCreate();
 //    $imageHtml->setImage('');
-    echo "<a href='$imageUrl/IMAGE/$imageName' target='new'><img src='$imageUrl/IMAGE/$imageName' title='$imageName' width=400px height=400px /></a> <br/>";
-    // echo "<label><input type='checkbox' name='$imageName' value='$imageName' /><span>削除する</span></label> <br/>";
-    echo 'アップロード日時: ' . date('Y/m/d H:i:s', $fileTime) . '<br/><br/>';
+    echo "<li>";
+    echo "<a href='$imageUrl/IMAGE/$imageName' target='new'><img src='$imageUrl/IMAGE/$imageName' title='$imageName' width=400px height=400px /></a>";
+    echo "<p class='image-info'>画像名: {$imageName}<br/>";
+    echo "アップロード日時: " . date('Y/m/d H:i:s', $fileTime) . "</p>";
+    echo "</li>";
 }
 
 /**
- * ViewList
- * 画像をリスト表示する
- *
- * @param  mixed $imageName
- * @param  mixed $imageUrl
- *
- * @return void
- */
+     * ViewList
+     * 画像をリスト表示する
+     *
+     * @param  mixed $imageName
+     * @param  mixed $imageUrl
+     *
+     * @return void
+     */
 function ViewList($imageName, $imageUrl) {
     echo "<div><a href='$imageUrl/IMAGE/$imageName' target='new'>{$imageName}</a>";
     echo "<label><input type='checkbox' name='$imageName' value='$imageName' /><span>削除する</span></label></div>";
