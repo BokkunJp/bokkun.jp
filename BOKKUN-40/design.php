b0781e6dc5a2836b9263765978d47a28a619a98e
ぼっくん <bokkun.moeanimecharacter.byt@gmail.com>
2021-02-20 16:07:55 +0900

    BOKKUN-40　【公開側|XML操作】XML読み込みフォームの新規作成
    XMLを読みこんで要素毎に出力するページを作成する。
    (表形式の出力はまだ)

--- BOKKUN-40/design.php
+++ BOKKUN-40/design.php
@@ -1,3 +1,6 @@
+<?php
+$session = new PublicSetting\Session();
+?>
 <form enctype="multipart/form-data" method='POST'>
     XMLファイルのアップロード：<input type='file' class='xml' name='xml' />
     <input type='hidden' name='xml-token' value="<?= $token = MakeToken() ?>" /> <button>アップロード</button>
