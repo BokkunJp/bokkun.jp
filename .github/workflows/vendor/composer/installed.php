da8f1a6467ac339dda31c95fcfcd7aac2a3b124b
ぼっくん <bokkun.moeanimecharacter.byt@gmail.com>
2022-02-20 02:52:48 +0900

    composer actionsのための調整
    試行錯誤

---
+++ .github/workflows/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b6fd99a79e8543de1e738b453aba041c0c6ce1ec',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b6fd99a79e8543de1e738b453aba041c0c6ce1ec',
+            'dev_requirement' => false,
+        ),
+    ),
+);
