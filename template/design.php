c4b0d6a413117e8f96823a3589fc9848bf0fd1b7
ぼっくん <admin@bokkun.jp>
2022-03-06 05:22:42 +0900

    smartyが正常に動作しないバグの修正
    smartyの読み込みができていなかったため修正。

--- template/design.php
+++ template/design.php
@@ -1,5 +1,14 @@
 <!-- デザイン用ファイル (PHPで処理を記述)-->
 <?php
-require_once(dirname(dirname(DOCUMENT_ROOT)). '/Plugin/smarty/core.php');
+// ini_set('error_reporting', 'E_ALL & ~E_NOTICE & ~E_DEPRECATED');
+SetPlugin('smarty');
+
+$smarty = new Smarty();
+
+$smarty->template_dir = './subdirectory/templates/';
+$smarty->compile_dir  = './subdirectory/templates_c/';
+$smarty->config_dir   = './subdirectory/configs/';
+$smarty->cache_dir    = './subdirectory/cache/';
+
 $smarty->assign('name', 'guest');
 $smarty->display('index.tpl');
