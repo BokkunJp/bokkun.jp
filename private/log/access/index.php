216efcf9c4d28a3930ed8814796cc2068788600d
ぼっくん <bokkun.moeanimecharacter.byt@gmail.com>
2022-04-16 23:40:54 +0900

    BOKKUN-ex 微調整
    管理側微調整。

--- private/log/access/index.php
+++ private/log/access/index.php
@@ -1,6 +1,4 @@
 <?php
 
-$homepageTitle = '管理画面';
 $title = 'アクセスログ';
-$ary = [$homepageTitle, $title];
 require_once('../../common/layout.php');
--- private/log/error/index.php
+++ private/log/error/index.php
@@ -1,6 +1,4 @@
 <?php
 
-$homepageTitle = '管理画面';
 $title = 'エラーログ';
-$ary = [$homepageTitle, $title];
 require_once('../../common/layout.php');
