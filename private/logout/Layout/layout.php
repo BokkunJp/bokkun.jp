150008cd5ff9402610eb94070459676bb4c44b98
ぼっくん <bokkun.moeanimecharacter.byt@gmail.com>
2023-01-09 15:24:33 +0900

    機能追加 管理側ログアウト
    ログアウト1秒後にトップページへリダイレクトする機能を追加。

--- private/logout/Layout/layout.php
+++ private/logout/Layout/layout.php
@@ -2,21 +2,21 @@
 
 $img = "crown-vector.jpg";
 ?>
+
 <html lang="ja">
 
 <head>
     <meta charset="utf-8">
     <meta name="robots" content="noindex,nofollow">
+    <meta http-equiv="refresh" content="0.5; url=<?=$base->GetUrl('private', relativePath:false)?>">
     <title>管理側</title>
-    <link rel="shortcut icon"
-        href="<?= $base->GetURL('', 'client') ?>image/IMG_7592.PNG">
-    <link rel="stylesheet" type="text/css"
-        href="<?= $base->GetURL('', 'client') ?>css/design.css">
+    <link rel="shortcut icon" href="<?= $base->GetUrl('', 'client') ?>image/IMG_7592.PNG">
+    <link rel="stylesheet" type="text/css" href="<?= $base->GetUrl('', 'client') ?>css/design.css">
 </head>
 
 <body>
     <link rel="stylesheet" type="text/css"
-        href="<?php echo $public; ?>client/css/common/<?php echo $agentCode; ?>.css">
+        href="<?= $base->GetUrl('', 'client') ?>css/common/<?php echo $agentCode; ?>.css">
     <div class="container">
         <?php require_once('header.php'); ?>
         <main class="contents">
