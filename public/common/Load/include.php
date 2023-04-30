5eef1fa6d534f6d8f3ad55fc78f4233bc6e175fb
ぼっくん <bokkun.moeanimecharacter.byt@gmail.com>
2023-05-01 03:56:41 +0900

    Vue.jsの修正
    CDNのバージョンをVue.js2系→Vue.js3系へ変更。
     Vue.config.devtoolsは使えなくなっているので削除。

--- public/common/Load/include.php
+++ public/common/Load/include.php
@@ -1,30 +1,22 @@
 <?php
 if (isset($ua)) {
     if ($ua->DesignJudge() === PC_design) {
-        echo "<script src='//code.jquery.com/jquery-3.6.0.min.js' integrity='sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=' crossorigin='anonymous'></script>";
-        echo "<script src='//code.jquery.com/ui/1.13.1/jquery-ui.min.js' integrity='sha256-eTyxS0rkjpLEo16uXTS0uVCS4815lc40K2iVpWDvdSY=' crossorigin='anonymous'></script>";
+        echo "<script src='https://code.jquery.com/jquery-3.6.0.min.js' integrity='sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=' crossorigin='anonymous'></script>";
+        echo "<script src='https://code.jquery.com/ui/1.13.1/jquery-ui.min.js' integrity='sha256-eTyxS0rkjpLEo16uXTS0uVCS4815lc40K2iVpWDvdSY=' crossorigin='anonymous'></script>";
 
         if ($homepageTitle === 'OpenCV') {
             echo "<script src='//docs.opencv.org/5.x/opencv.js'></script>";
         }
 
-        if (public\Setting::GetServerName() === 'bokkun.jp.local') {
-            echo "<script src='//jp.vuejs.org/js/vue.js'></script>";
-        } else {
-            echo "<script src='//jp.vuejs.org/js/vue.min.js'></script>";
-        }
+        echo "<script src='//unpkg.com/vue@3.2.47/dist/vue.global.js'></script>";
     } elseif ($ua->DesignJudge() === SP_design) {
-        echo "<script src='//code.jquery.com/jquery-3.6.0.min.js' integrity='sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=' crossorigin='anonymous'></script>";
-        echo "<script src='//code.jquery.com/ui/1.13.1/jquery-ui.min.js' integrity='sha256-eTyxS0rkjpLEo16uXTS0uVCS4815lc40K2iVpWDvdSY=' crossorigin='anonymous'></script>";
+        echo "<script src='https://code.jquery.com/jquery-3.6.0.min.js' integrity='sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=' crossorigin='anonymous'></script>";
+        echo "<script src='https://code.jquery.com/ui/1.13.1/jquery-ui.min.js' integrity='sha256-eTyxS0rkjpLEo16uXTS0uVCS4815lc40K2iVpWDvdSY=' crossorigin='anonymous'></script>";
 
         if ($homepageTitle === 'OpenCV') {
-            echo "<script src='//docs.opencv.org/5.x/opencv.js'></script>";
+            echo "<script src='https://docs.opencv.org/5.x/opencv.js'></script>";
         }
 
-        if (public\Setting::GetServerName() === 'bokkun.jp.local') {
-            echo "<script src='//jp.vuejs.org/js/vue.js'></script>";
-        } else {
-            echo "<script src='//jp.vuejs.org/js/vue.min.js'></script>";
-        }
+        echo "<script src='https://unpkg.com/vue@next'></script>";
     }
 }
