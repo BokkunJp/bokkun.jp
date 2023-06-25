4f7aacca700b6841359e6657c31e1c3198069dac
ぼっくん <bokkun.moeanimecharacter.byt@gmail.com>
2023-06-26 02:19:57 +0900

    Vue.jsの修正
    CDNのバージョンをVue.js2系→Vue.js3系へ変更。
     Vue.config.devtoolsは使えなくなっているので削除。

--- common/error/common/Include.php
+++ common/error/common/Include.php
@@ -8,12 +8,7 @@ require_once(__DIR__ . "/Function/UA.php");
 // jQuery, Vue.js
 echo "<script src='https://code.jquery.com/jquery-3.6.0.min.js' integrity='sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=' crossorigin='anonymous'></script>";
 echo "<script src='https://code.jquery.com/ui/1.13.1/jquery-ui.min.js' integrity='sha256-eTyxS0rkjpLEo16uXTS0uVCS4815lc40K2iVpWDvdSY=' crossorigin='anonymous'></script>";
-
-if (ErrorSetting\Setting::GetServerName() === 'bokkun.jp.local') {
-    echo "<script src='//unpkg.com/vue@3.2.47/dist/vue.global.js'></script>";
-} else {
-    echo "<script src='//jp.vuejs.org/js/vue.min.js'></script>";
-}
+echo "<script src='https://unpkg.com/vue@next'></script>";
 
 
 /**
