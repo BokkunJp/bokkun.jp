737d06d59559fc96f4dd12a982eed64ce3188b22
ぼっくん <bokkun.moeanimecharacter.byt@gmail.com>
2023-12-29 04:08:08 +0900

    バックログ用のファイルの削除

--- BOKKUN-24/subdirectory/Form.php
+++
@@ -1,9 +0,0 @@
-<?php
-$basePath = new \Path(__DIR__);
-$basePath->add("Base");
-includeFiles($basePath->get(), 'php', false, ['BaseValid']);
-class Valid extends BaseVaild
-{
-}
-
-$valid = new Valid();
