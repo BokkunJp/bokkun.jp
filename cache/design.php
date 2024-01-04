0e27f1b9a73c481d131d287f5cb1e0353624c1c8
βοκκμη <bokkun.moeanimecharacter.byt@gmail.com>
2024-01-05 06:32:54 +0900

    cacheページの修正
    cacheページの内容修正

--- cache/design.php
+++ cache/design.php
@@ -5,18 +5,24 @@
 
 // $cache = new \Cache('test');
 
+use Public\Important\Setting;
 use Public\Important\Session;
 use Public\Important\Token;
 
 $name = "cache-csrf";
 $session = new Session($name);
-$csrf = new Token($name, $session);
+$csrf = new Token($name, $session, true);
 
-if (!$csrf->check()) {
-    echo "<div class='warning'>不正な遷移です。</div>";
+if (!is_null(Setting::getPosts())) {
+    if (!$csrf->check()) {
+        echo "<div class='warning'>不正な遷移です。</div>";
+    } else {
+        echo "<div class='success'>正常に送信されました。</div>";
+    }
 }
+
 ?>
 <form action='./' method='POST'>
     <button>ボタンを押してね！</button>
-    <?=$csrf->getTag()?>
+    <?=$csrf->set()?>
 </form>
