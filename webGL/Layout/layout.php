98e50698d2fd082846ac6a3fd9b0ba8aa04d489a
βοκκμη <private.mail@bokkun.jp>
2024-05-19 06:18:23 +0900

    bokkun-SESSION セッション周りの再修正
    処理の見直し・全体の再修正

--- webGL/Layout/layout.php
+++ webGL/Layout/layout.php
@@ -1,46 +1,23 @@
-<?php
-
-// セッションスタート
-if (!isset($_SESSION)) {
-    if (PHP_OS === 'WINNT') {
-        $sessionDir = dirname(filter_input(INPUT_SERVER, 'DOCUMENT_ROOT')). "/var/";
-        if (!is_dir($sessionDir)) {
-            mkdir($sessionDir, 0755);
-            $sessionDir = dirname(filter_input(INPUT_SERVER, 'DOCUMENT_ROOT')). "/var/session/";
-            if (!is_dir($sessionDir)) {
-                mkdir($sessionDir, 0755);
-            } else {
-                $sessionDir .= '/session/';
-            }
-        }
-        session_save_path($sessionDir);
-    }
-    session_start();
-} else {
-    session_reset();
-}
-
-?>
 <!DOCTYPE html>
 <?php
 
 require_once __DIR__ . '/require.php';
 
-$title = "WebGL";
+$title = basename(getcwd());
 $img = "crown-vector.jpg";
 ?>
-<!DOCTYPE html>
 <html lang="ja">
 
 <head>
+    <meta charset="utf-8">
+    <meta name="robots" content="noindex,nofollow">
     <title><?php echo $title; ?>
     </title>
     <base href="../" />
-    <link rel="shortcut icon"
-        href="<?php echo $public; ?>client/image/IMG_7592.PNG">
+    <link rel="shortcut icon" href="/public/client/image/IMG_7592.PNG">
+    <?php require_once dirname(__DIR__, 2) . '/public/common/Layout/init.php'; ?>
     <link rel="stylesheet" type="text/css"
-        href="<?php echo $public; ?>client/css/webGL/design.css">
-    <?php require_once __DIR__ . '/init.php'; ?>
+        href="/public/client/css/<?php echo $homepageTitle; ?>/design.css">
 </head>
 
 <body>
@@ -49,7 +26,7 @@ $img = "crown-vector.jpg";
     <div class="container">
         <?php require_once('header.php'); ?>
         <main class="contents">
-            <?php require_once('design.php'); ?>
+            <?php require_once(getcwd() . '/design.php'); ?>
         </main>
         <?php require_once('footer.php'); ?>
     </div>
