2391e714a69c1ce24ccc67a7e18ca30e1f2d77dc
βοκκμη <private.mail@bokkun.jp>
2024-09-07 01:03:41 +0900

    ファイル構成の変更
    desin.php以外にもコンテンツファイルを読み込めるように修正。

--- private/Sample/Layout/layout.php
+++ private/Sample/Layout/layout.php
@@ -43,8 +43,17 @@ $img = "0113.png";
     <div class="container">
         <?php require_once('header.php'); ?>
         <main class="contents">
-            <?php require_once(getcwd() . '/design.php'); ?>
-        </main>
+            <?php
+                if (isset($contents)) {
+                    // コンテンツが指定されている
+                    output($contents);
+                }
+
+                // index.phpと同階層にあるファイルを読み込む
+                $contentsPath = getcwd(). DIRECTORY_SEPARATOR;
+                IncludeFiles($contentsPath);
+            ?>
+         </main>
         <?php require_once('footer.php'); ?>
     </div>
 </body>
