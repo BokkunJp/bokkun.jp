6d9d5eef528b85e739ecdcd8f6c5a8dd1a3dbe1f
βοκκμη <private.mail@bokkun.jp>
2024-12-07 20:31:29 +0900

    微調整
    古くから放置していたエラーの修正。

--- private/common/Component/function.php
+++ private/common/Component/function.php
@@ -34,13 +34,16 @@ class Setting
     protected function allowAuthoritys($authority)
     {
         $key = array_keys($this->authorities, $authority);
-        $this->authorities = array_splice($this->authorities, $key, 1);
+        foreach ($key as $_key) {
+            if (is_string($_key) || is_numeric($_key)) {
+                $this->authorities = array_splice($this->authorities, $_key, 1);
+            }
+        }
     }
 
     protected function allowAuthority($authority)
     {
-        $key = array_keys($this->authorities, $authority);
-        $this->authorities = array_splice($this->authorities, $key, 1);
+        $this->allowAuthoritys([$authority]);
     }
 
     public function setDefault($authority)
