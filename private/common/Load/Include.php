d72ee81392e35fd08c4cc5ab5db290634af82562
βοκκμη <private.mail@bokkun.jp>
2025-01-26 03:27:25 +0900

    jQueryUI更新
    jQueryUIを1.13.3から1.14.1へ更新。

--- private/common/Load/Include.php
+++ private/common/Load/Include.php
@@ -1,7 +1,7 @@
 <?php
 if (!isset($ajaxFlg)) {
     echo "<script src='https://code.jquery.com/jquery-3.7.1.min.js' ></script>";
-    echo "<script src='https://code.jquery.com/ui/1.13.3/jquery-ui.min.js' ></script>";
+    echo "<script src='https://code.jquery.com/ui/1.14.1/jquery-ui.min.js' ></script>";
     if (Private\Important\Setting::getServerName() === 'bokkun.jp.local') {
         // echo "<script src='//unpkg.com/vue@3.2.47/dist/vue.global.js'></script>";
     } else {
