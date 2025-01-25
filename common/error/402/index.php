ca1ee3d55ce4812c791c8d1ed683a366455445c1
βοκκμη <private.mail@bokkun.jp>
2025-01-26 03:09:57 +0900

    HTTPステータス関連の調整
    418(ジョークコード)含め、ステータスコード関連の調整。

---
+++ common/error/402/index.php
@@ -0,0 +1,6 @@
+<?php
+
+// 初期設定を記述
+$homepageTitle = htmlspecialchars(basename(__DIR__));
+http_response_code(402);
+require_once dirname(__DIR__). '/common/Layout/layout.php';
---
+++ common/error/409/design.php
@@ -0,0 +1,5 @@
+<!-- デザイン用ファイル (PHPで処理を記述)-->
+<?php
+if (isset($message)) {
+    echo $message;
+}
