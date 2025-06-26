204144388875ae0ebc72afd265b191882f751bcf
βοκκμη <private.mail@bokkun.jp>
2025-06-27 01:01:44 +0900

    imageページの処理修正
    表示枚数が正常に動作しなくなったので修正。

--- image/subdirectory/ajax/server.php
+++ image/subdirectory/ajax/server.php
@@ -1,13 +1,13 @@
 <?php
 
 header("Content-Type: application/json; charset=UTF-8");
+chdir(dirname(getcwd(), 2));
 require_once dirname(__DIR__, 3). "/public/common/ajax-require.php";
 
 $imagePath = new \Path(PUBLIC_COMMON_DIR);
 $imagePath->add('image');
 includeFiles($imagePath->get());
 
-chdir(dirname(getcwd(), 2));
 
 $imageResult = readImage(ajaxFlg:true);
 
